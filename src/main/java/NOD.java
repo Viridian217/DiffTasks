1607b87dae5b7c86fff83588e68449e6b32e9c49
Budnikov Illia <ilyabudkov@gmail.com>
2022-08-14 16:23:17 +0300

    Add new solving

---
+++ src/main/java/NOD.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class NOD {
+
+    /*
+    НОД
+    */
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(NOD(scanner.nextInt(), scanner.nextInt()));
+    }
+
+    public static int NOD(int num1, int num2) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 1; i <= num1; i++) {
+            if (num1 % i == 0) {
+                set.add(i);
+            }
+        }
+        int[] max = {0};
+        set.forEach(o -> {
+            if (num2 % o == 0) {
+                if (max[0] < o) {
+                    max[0] = o;
+                }
+            }
+        });
+        return max[0];
+
+    }
+}
