43bf41eaf5f7fb7c233eedc611c6b216ee053381
Budnikov Illia <ilyabudkov@gmail.com>
2022-09-12 15:58:42 +0300

    training in pattern stream, write in file pair symbols

---
+++ src/main/java/reader/PairSymbolsWriter.java
@@ -0,0 +1,20 @@
+package reader;
+
+import java.io.*;
+
+public class PairSymbolsWriter {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileReader fileReader = new FileReader(reader.readLine());
+             FileWriter fileWriter = new FileWriter(reader.readLine())) {
+            int count = 1;
+            while (fileReader.ready()) {
+                char elem = (char) fileReader.read();
+                if (count % 2 == 0) {
+                    fileWriter.write(elem);
+                }
+                count++;
+            }
+        }
+    }
+}
