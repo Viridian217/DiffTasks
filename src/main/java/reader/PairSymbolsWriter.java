f353992205262721eec6c9a73b4305ebbb866fb8
Budnikov Illia <ilyabudkov@gmail.com>
2022-09-12 15:59:01 +0300

    training in stream, write in file pair symbols

--- src/main/java/reader/PairSymbolsWriter.java
+++ src/main/java/reader/PairSymbolsWriter.java
@@ -6,7 +6,7 @@ public class PairSymbolsWriter {
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
              FileReader fileReader = new FileReader(reader.readLine());
-             FileWriter fileWriter = new FileWriter(reader.readLine())) {
+             FileWriter fileWriter = new FileWriter(reader.readLine())){
             int count = 1;
             while (fileReader.ready()) {
                 char elem = (char) fileReader.read();
