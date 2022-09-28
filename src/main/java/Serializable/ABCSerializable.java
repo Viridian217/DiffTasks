d8b5e934de77061f0058edbbe599e9877e7363a8
Budnikov Illia <ilyabudkov@gmail.com>
2022-09-28 19:21:25 +0300

    training in serializable, this problem from javarush

---
+++ src/main/java/Serializable/ABCSerializable.java
@@ -0,0 +1,40 @@
+package Serializable;
+
+import java.io.Serializable;
+
+public class ABCSerializable {
+    public class A implements Serializable {
+        String name = "A";
+
+        public A(String name) {
+            this.name += name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    public class B extends A {
+        String name = "B";
+
+        public B(String name) {
+            super(name);
+            this.name += name;
+        }
+    }
+
+    public class C extends B {
+        String name = "C";
+
+        public C(String name) {
+            super(name);
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
